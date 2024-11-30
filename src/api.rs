0a7c1e5a8897e0ee721984d71d730e03a48be269
iltifatibad <151670452+iltifatibad@users.noreply.github.com>
2024-11-30 22:16:19 +0000

    Update api.rs

--- src/api.rs
+++ src/api.rs
@@ -4,10 +4,8 @@ use r2d2_sqlite::SqliteConnectionManager;
 use rusqlite::{Connection, params};
 
 pub async fn add_data(pool: web::Data<Pool<SqliteConnectionManager>>) -> Result<HttpResponse, Error> {
-    // Havuzdan bir bağlantı alıyoruz
     let conn = pool.get().unwrap();
     
-    // Veritabanına veri ekleme işlemi
     let result = conn.execute("INSERT INTO users (name) VALUES (?1)", params!["Pepe"]);
     
     match result {
